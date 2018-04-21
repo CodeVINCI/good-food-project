24e22ea7688afe98aeb50333da14b54337b2c189
shrikrishna <15bcs044@smvdu.ac.in>
2018-04-21 16:19:01 +0530

    Improvements
    This improve README.md to setup postgresql

--- goodfood/goodfood/settings.py
+++ goodfood/goodfood/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'goodfood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'), #os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
